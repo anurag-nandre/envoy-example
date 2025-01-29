3ff9644cd9f884935c1b342f2fa54c6280c901f8
Anurag Nandre <anandre@twilio.com>
2025-01-29 15:22:59 -0600

    enhancements

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Define environment variable
 ENV FLASK_APP=service_a.py
-
+ENV PORT=8080
 CMD ["python", "servicea.py"]
--- servicea.py
+++ servicea.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 SERVICE_B_URL = "http://localhost:10000"  # Envoy proxy for Service B
-SERVICE_B_URL = "http://localhost:10000"
+
 @app.route('/')
 def home():
     return "Hello from Service A!"
@@ -27,5 +27,15 @@ def call_serviceC():
     except requests.exceptions.RequestException as e:
         return jsonify(message="Request to Service B failed", error=str(e)), 500
 
+@app.route('/healthcheck', methods=['GET'])
+def call_service_healthcheck():
+    try:
+        response = requests.get(f"{SERVICE_B_URL}/healthcheck", timeout=5)
+        return jsonify(message="Request to  healthcheck succeeded", serviceB_response=response.json()), response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify(message="Request to healthcheck failed", error=str(e)), 500
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Service A listens on port
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)  # Service B listens on the specified port
+
